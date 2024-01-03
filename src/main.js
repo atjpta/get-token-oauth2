29aae4c1a51117e1fefd9f4b439c0538e4a167ae
atjpta <caoducan351@gmail.com>
2024-01-03 23:21:21 +0700

    fix when null clientid gg

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import 'vue-toast-notification/dist/theme-bootstrap.css';
 import GoogleSignInPlugin from "vue3-google-signin"
 
-const valueClientIdGg = localStorage.getItem('ClientIdGg');
+const valueClientIdGg = localStorage.getItem('ClientIdGg') ?? '';
 const app = createApp(App)
 app.use(GoogleSignInPlugin, {
     clientId: valueClientIdGg,
